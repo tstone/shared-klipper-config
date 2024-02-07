7556fa2f5e44e3af34a0060ed0a30fb1b72909f6
Titus <wastingtape@gmail.com>
2024-02-06 22:03:23 -0700

    Update update-bridge-mcu.sh

--- scripts/update-bridge-mcu.sh
+++ scripts/update-bridge-mcu.sh
@@ -2,19 +2,20 @@
 set -e
 
 # Run as:
-# KCONFIG_CONFIG=name ./update_can_bridge_mcu.sh ca5b098aba5b usb-katapult_stm32f429xx_2E0022001651323039323733-if00
+# ./update_can_bridge_mcu.sh name ca5b098aba5b usb-katapult_stm32f429xx_2E0022001651323039323733-if00
 
 # References:
 # https://github.com/Esoterical/voron_canbus/blob/main/mainboard_flashing/README.md
 # https://github.com/muyouhcd/klipper/blob/master/docs/gcode_shell_command.md
 
-CAN_UUID=$1
-SERIAL=$2
+CONFIG=$1
+CAN_UUID=$2
+SERIAL=$3
 
 # build new firmware
 cd ~/klipper
 make clean
-make KCONFIG_CONFIG=$KCONFIG_CONFIG
+make KCONFIG_CONFIG=$CONFIG
 
 # stop Klipper
 sudo service klipper stop
